aa93a539e923bae54f1d42ab1e7b83864231b7ed
Federico López <fede520@live.com>
2023-11-07 20:03:45 -0300

    Operaciones PUT , POST , DELETE

--- backend/users.py
+++ backend/users.py
@@ -29,9 +29,46 @@ async def user_path(id : int):
 async def user_query(id : int):
     return search_user(id)
 
+
+@app.post("/new-user")
+async def new_user(user : User):
+    
+    if type(search_user(user.id)) == User:
+        return {"Error": "El usuario ya existe."}
+    
+    users_list.append(user)
+    return user
+
+@app.put("/modify-user") # para todo el registro se usa PUT y para alguna parte del dato #PATCH
+async def modify_user(user : User):
+    
+    found = False
+    if type(search_user(user.id)) == User:
+        for index,saved_user in enumerate(users_list):
+            if saved_user.id == user.id :
+                users_list[index] = user
+                found = True
+    
+    if found == False:
+        return {"Error" : "El usuario no se ha encontrado"}
+    
+    return user
+
+@app.delete("/delete-user/{id}")
+async def delete_user(id : int):
+    deleted = False  
+    for index,saved_user in enumerate(users_list):
+            if saved_user.id == id :
+                del users_list[index]
+                deleted = True
+    if deleted == False:
+        return {"Error" : "Usuario no encontrado."}
+    return {"Ok": "Usuario eliminado"}
+
 def search_user(id : int):
     try :
        users = filter(lambda user: user.id == id, users_list)
        return list(users)[0] 
     except : 
         return {"Error":f"El usuario con id {id} no se encuentra en la lista"}
+    
